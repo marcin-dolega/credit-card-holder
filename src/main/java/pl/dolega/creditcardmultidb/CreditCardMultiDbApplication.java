98c527797859364d94e7040ef77945692fe6698e
Marcin Dolega <marcin92d@gmail.com>
2022-12-08 15:48:46 +0100

    Initial Commit

---
+++ src/main/java/pl/dolega/creditcardmultidb/CreditCardMultiDbApplication.java
@@ -0,0 +1,13 @@
+package pl.dolega.creditcardmultidb;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class CreditCardMultiDbApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(CreditCardMultiDbApplication.class, args);
+	}
+
+}
---
+++ src/test/java/pl/dolega/creditcardmultidb/CreditCardMultiDbApplicationTests.java
@@ -0,0 +1,13 @@
+package pl.dolega.creditcardmultidb;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class CreditCardMultiDbApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
