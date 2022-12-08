9d36841b06b72bc5999624da5b0f10ee118fd093
Marcin Dolega <marcin92d@gmail.com>
2022-12-08 16:48:42 +0100

    Data Source Connections

--- src/main/java/pl/dolega/creditcardmultidb/config/CardDatabaseConfiguration.java
+++ src/main/java/pl/dolega/creditcardmultidb/config/CardDatabaseConfiguration.java
@@ -1,16 +1,28 @@
 package pl.dolega.creditcardmultidb.config;
 
+import com.zaxxer.hikari.HikariDataSource;
+import org.springframework.beans.factory.annotation.Qualifier;
 import org.springframework.boot.autoconfigure.jdbc.DataSourceProperties;
 import org.springframework.boot.context.properties.ConfigurationProperties;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
+
+import javax.sql.DataSource;
 
 @Configuration
 public class CardDatabaseConfiguration {
 
     @Bean
     @ConfigurationProperties("spring.card.datasource")
-    public DataSourceProperties cardHolderDataSourceProperties() {
+    public DataSourceProperties cardDataSourceProperties() {
         return new DataSourceProperties();
+    }
+
+    @Bean
+    public DataSource cardDataSource(
+            @Qualifier("cardDataSourceProperties") DataSourceProperties cardDataSourceProperties) {
+        return cardDataSourceProperties.initializeDataSourceBuilder()
+                .type(HikariDataSource.class)
+                .build();
     }
 }
